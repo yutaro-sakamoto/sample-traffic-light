0d499480becfc54fb0564578f58a423faaf5dbd2
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2023-10-26 07:52:21 +0900

    [Add]: add the first sample code

---
+++ src/light.rs
@@ -0,0 +1,6 @@
+#[derive(Clone, Debug)]
+pub enum Light {
+    Red(u32),
+    Yellow(u32),
+    Blue(u32)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod light;
+
+use light::Light;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Yellow 1: {:?}", Light::Yellow(1));
+    println!("Blue 2: {:?}", Light::Blue(2));
+    println!("Red 3: {:?}", Light::Red(3));
 }
